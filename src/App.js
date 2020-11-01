d8c96302d7097667dc38f233e02daba339e94c4f
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-11-01 21:52:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [thought, setThought] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const thoughts = ["wow", "I am travelling in time."];
+    const timerID = setInterval(() => tick(), 1000);
+    let i = 0;
+    const statements = setInterval(() => {
+      if (i < thoughts.length) {
+        setThought(thoughts[i]);
+        setVisible(true); 
+      } else return;
+      i++;
+    }, 5000);
+
+    return () => {
+      clearInterval(timerID);
+      clearInterval(statements);
+    };
+  }, []);
+
+  const tick = () => {
+    setDate(new Date());
+  };
+
+  setInterval(() => tick(), 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class={visible? "bubble thought": "invisible"}>
+        <h2>{thought}</h2>
+      </div>
+      <div className="clock">
+        <h2>{date.toLocaleTimeString()}</h2>
+      </div>
     </div>
   );
 }
